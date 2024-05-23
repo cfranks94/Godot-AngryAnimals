56b2e199302fb9999171cc3feb7ecefee2e896ef
Cord Franks <crfranks94@gmail.com>
2024-05-22 19:52:16 -0500

    game ui and game over functionality

--- scenes/scorer/scorer.gd
+++ scenes/scorer/scorer.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var _attemps: int = 0
+var _attempts: int = 0
 var _cups_hit: int = 0
 var _total_cups: int = 0
 var _level_number: int = 1
@@ -15,10 +15,12 @@ func _ready():
 
 
 func on_attempt_made() -> void:
-	_attemps += 1
+	_attempts += 1
+	SignalManager.on_score_updated.emit(_attempts)
 
 
 func on_cup_destroyed() -> void:
 	_cups_hit += 1
 	if _cups_hit == _total_cups:
-		pass
+		SignalManager.on_level_complete.emit()
+		ScoreManager.set_score_for_level(_attempts, str(_level_number))
